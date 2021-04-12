1da87d895d9445dfea3784841caeb024ed4adcd2
Nikolas Theodoropoulos <nikolastheodoropoulos@Nikolass-MacBook-Air.local>
2021-04-12 17:37:26 +0300

    added monkey animation

--- src/threeD/Lights.tsx
+++ src/threeD/Lights.tsx
@@ -4,11 +4,26 @@ import Main from './Main';
 class Lights {
 
     private main: Main;
+    public monkeyLightAtStartOfTheAnimation!: THREE.SpotLight;
     public monkeyLight!: THREE.SpotLight;
     private monkeyHelper!: THREE.SpotLightHelper;
 
     constructor(main: Main) {
         this.main = main;
+    }
+
+    private createMonkeyLightAtStartOfTheAnimation(): void {
+        this.monkeyLightAtStartOfTheAnimation = new THREE.SpotLight(0x00ff00, 10, 100, Math.PI / 10, .4, 1);
+        this.monkeyLightAtStartOfTheAnimation.position.set(0, 10, 0);
+        const targetObject = new THREE.Object3D();
+        targetObject.position.set(0, 0, -20);
+        this.main.scene.add(targetObject);
+        this.monkeyLightAtStartOfTheAnimation.target = targetObject;
+        this.main.scene.add(this.monkeyLightAtStartOfTheAnimation);
+        
+
+        const helper = new THREE.SpotLightHelper(this.monkeyLightAtStartOfTheAnimation);
+        // this.main.scene.add(helper);
     }
 
     private createMonkeyLight(): void {
@@ -22,6 +37,7 @@ class Lights {
 
     public init():void {
         this.createMonkeyLight();
+        this.createMonkeyLightAtStartOfTheAnimation();
     }
 
     public updateHelpers():void {
