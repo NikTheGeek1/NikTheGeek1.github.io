98d3ef18369e4dff48739a2f6c2dc79546cdc7b0
nikthegeek1 <ntheodoropoulos@outlook.com>
2021-05-03 10:11:06 +0300

    added skills seminars and languages in experience

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -5,6 +5,8 @@ import educationIcon from '../../assets/svgs/education.svg';
 import workIcon from '../../assets/svgs/work-gear.svg'
 import DetailsItem from './DetailsItem/DetailsItem';
 import TagBubble from '../TagBubble/TagBubble';
+import seminarIcon from '../../assets/svgs/seminars.svg';
+import skillsIcon from '../../assets/svgs/skills.svg';
 
 const Experience = () => {
 
@@ -94,12 +96,12 @@ const Experience = () => {
                         <>
                             <DetailsItem>Main duties: Analysis and writing up the results of 4 experiments on exploring which reference frames are being used in the automatic spatial coding of touch to fingers</DetailsItem>
                             <DetailsItem>Two conferences:
-                                <ol style={{marginLeft: "3rem"}}>
+                                <ol style={{ marginLeft: "3rem" }}>
                                     <li>The multifaceted body: updates into Body Representation and Embodiment, Edinburgh June 29</li>
                                     <li>International Multisensory Research Forum (IMRF), Toronto, June 14-17</li>
                                 </ol>
                             </DetailsItem>
-                            <DetailsItem><TagBubble title="Article" link="https://doi.org/10.1016/j.actpsy.2019.102874"/></DetailsItem>
+                            <DetailsItem><TagBubble title="Article" link="https://doi.org/10.1016/j.actpsy.2019.102874" /></DetailsItem>
                         </>
                     ]}
                 />
@@ -114,6 +116,97 @@ const Experience = () => {
                     subtitle="Sismanoglio General Hospital, Greece"
                     subsubtitle="Child and Adolescent Psychiatry"
                     year="3-4/2015"
+                />
+            </ExperienceSection>
+
+
+            <ExperienceSection icon={seminarIcon} title="Seminars & online courses">
+                <ExperienceSectionItem
+                    title="Probabilistic Graphical Models specialisation"
+                    subtitle="Stanford Online"
+                    subsubtitle="Graduated with hours in 2 out of 3 courses"
+                    year="6 months in 2018"
+                />
+                <ExperienceSectionItem
+                    title="Neural Networks and Deep Learning specialisation"
+                    subtitle="Stanford Online"
+                    subsubtitle="So far 2/5 courses"
+                    year="4 months in 2019"
+                />
+                <ExperienceSectionItem
+                    title="Bayesian Stats Club"
+                    subtitle="The University of Edinburgh"
+                    subsubtitle="Optional reading group focusing on Kruschke's: Doing Bayesian Data Analysis "
+                    year="2017-2018"
+                />
+                <ExperienceSectionItem
+                    title="Research workshop in NLP & Machine Learning"
+                    subtitle="The University of Edinburgh"
+                    subsubtitle=""
+                    year="04/2017"
+                />
+            </ExperienceSection>
+
+            <ExperienceSection icon={skillsIcon} title="Skills">
+            <ExperienceSectionItem
+                    title="Programming and Markup Languages"
+                    subtitle=""
+                    subsubtitle=""
+                    year=""
+                    children={[
+                        <>
+                            <DetailsItem>Confident with:
+                                <TagBubble title="JavaScript" />
+                                <TagBubble title="Python" />
+                                <TagBubble title="Java" />
+                                <TagBubble title="R" />
+                                <TagBubble title="MATLAB" />
+                                <TagBubble title="HTML/CSS/SASS" />
+                                <TagBubble title="Latex" />
+                                <TagBubble title="C++" />
+                            </DetailsItem>
+                        </>
+                    ]}
+                />
+                <ExperienceSectionItem
+                    title="Frameworks/Technologies"
+                    subtitle=""
+                    subsubtitle=""
+                    year=""
+                    children={[
+                        <>
+                            <DetailsItem>Worked with:
+                                <TagBubble title="Node.js" />
+                                <TagBubble title="React" />
+                                <TagBubble title="Reac Native" />
+                                <TagBubble title="Node.js" />
+                                <TagBubble title="Flask" />
+                                <TagBubble title="WebGL" />
+                                <TagBubble title="Socket.io" />
+                                <TagBubble title="STOMP" />
+                                <TagBubble title="Spring" />
+                                <TagBubble title="Hibernate" />
+                                <TagBubble title="Relational databases (PostgreSQL, SQLite...)" />
+                                <TagBubble title="Non-relational databases (Firebase, MongoDB)" />
+                                <TagBubble title="Visualisation (matplotlib, plotly...)" />
+                                <TagBubble title="Data handling/analysis (numpy, scipy, pandas, tensorflow...)" />
+                            </DetailsItem>
+                        </>
+                    ]}
+                />
+                <ExperienceSectionItem
+                    title="Languages"
+                    subtitle=""
+                    subsubtitle=""
+                    year=""
+                    children={[
+                        <>
+                            <DetailsItem>
+                                <TagBubble title="Greek" />
+                                <TagBubble title="English" />
+                            </DetailsItem>
+                        </>
+                    ]}
                 />
             </ExperienceSection>
         </>
