9b6872bb9217f252efe9ba5e983806edb22f2451
nikthegeek1 <ntheodoropoulos@outlook.com>
2021-05-06 16:59:42 +0300

    fixing reduced image

--- src/components/ReducedImage/ReducedImage.tsx
+++ src/components/ReducedImage/ReducedImage.tsx
@@ -1,24 +1,26 @@
 import { useState } from 'react';
 import './ReducedImage.css';
 
-const ReducedImage = ({reducedImage, normalImage}: {
+const ReducedImage = ({ reducedImage, normalImage }: {
     reducedImage: string,
     normalImage: string
 }) => {
-    const [normalImageLoaded, setNormalImageLoaded] = useState(false);
     const [reducedImageLoaded, setReducedImageLoaded] = useState(false);
+
     const reducedImageLoadedHandler = () => {
         setReducedImageLoaded(true);
     };
-    
-    const normalImageLoadedHandler = () => {
-        setNormalImageLoaded(true);
-    };
+
+
 
     return (
         <>
-            { !reducedImageLoaded && <img src={normalImage} className="photos-photo" onLoad={normalImageLoadedHandler}/> }
-            { !normalImageLoaded && <img src={reducedImage} className="photos-photo-reduced" onLoad={reducedImageLoadedHandler} /> }
+            { reducedImageLoaded && <img src={normalImage} className="photos-photo" />}
+            <img
+                src={reducedImage}
+                className="photos-photo-reduced"
+                onLoad={reducedImageLoadedHandler} 
+            />
         </>
     );
 };
