52a8e7c709c63d8d27a2ec3a549b36eb34abeda6
nikthegeek1 <ntheodoropoulos@outlook.com>
2021-06-01 19:00:58 +0300

    fixing files to download

--- src/imports/import-photo-gallery-blog-files.tsx
+++ src/imports/import-photo-gallery-blog-files.tsx
@@ -2,7 +2,7 @@ import appDotTsx from '../blog-files-to-download/photo-gallery-blog/App.tsx.txt'
 import appDotCss from '../blog-files-to-download/photo-gallery-blog/App.css.txt';
 import indexDotTsx from '../blog-files-to-download/photo-gallery-blog/Index.tsx.txt';
 import indexDotCss from '../blog-files-to-download/photo-gallery-blog/Index.css.txt';
-import reactAppEnvDotTs from '../blog-files-to-download/photo-gallery-blog/react-app-env.d.ts.txt'
+import reactAppEnvDotTs from '../blog-files-to-download/photo-gallery-blog/react-app-env.d.ts.txt';
 
 const imports = {
     appDotTsx,
@@ -13,3 +13,4 @@ const imports = {
 };
 
 export default imports;
+
