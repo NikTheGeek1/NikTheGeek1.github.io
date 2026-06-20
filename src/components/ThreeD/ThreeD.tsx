fa9d1661074327adc1d5539104bbc5b117c37142
NikTheGeek1 <ntheodoropoulos@outlook.com>
2026-06-20 09:43:27 +0300

    Keep Suzanne stable in profile view

--- src/components/ThreeD/ThreeD.tsx
+++ src/components/ThreeD/ThreeD.tsx
@@ -1,5 +1,4 @@
 import { createRef, useEffect, useRef, useState } from 'react';
-import { useLocation } from 'react-router-dom';
 import './ThreeD.css';
 import Main from '../../threeD/Main';
 import Monkey from 'src/threeD/Monkey';
@@ -8,7 +7,6 @@ const ThreeD = ({ setMonkeyClicked, playExitAnimation = false }: { setMonkeyClic
   const [monkeyInstance, setMonkeyInstance] = useState<Monkey | null>(null);
   const exitAnimationStarted = useRef<boolean>(false);
   const canvasRef = createRef<HTMLCanvasElement>();
-  const location = useLocation();
 
   useEffect(() => {
     if (canvasRef && canvasRef.current) {
@@ -43,19 +41,6 @@ const ThreeD = ({ setMonkeyClicked, playExitAnimation = false }: { setMonkeyClic
 
     return () => window.clearInterval(retryTimer);
   }, [playExitAnimation, monkeyInstance]);
-
-  useEffect(() => {
-    // get path parameter from the URL
-    const params = new URLSearchParams(location.search);
-    const path = params.get("path");
-    const isDeepLink = location.pathname !== "/" || path || params.get("project");
-    // if there is a path parameter, click on the monkey
-    if (isDeepLink && monkeyInstance) {
-      setTimeout(() => {
-        monkeyInstance.onClick(true);
-      }, 2000);
-    }
-  }, [location.pathname, location.search, monkeyInstance]);
 
   return (
     <canvas ref={canvasRef} className="canvas"></canvas>
